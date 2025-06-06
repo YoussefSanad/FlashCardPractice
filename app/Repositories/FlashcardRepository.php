96859fae83ccb7e7530320a33aac109138c40822
youssef <y.sanad@trengo.com>
2025-06-06 20:29:20 +0200

    fix: correct typo in FlashcardRepository interface method parameter

--- app/Repositories/FlashcardRepository.php
+++ app/Repositories/FlashcardRepository.php
@@ -6,7 +6,7 @@ use App\Models\Flashcard;
 
 interface FlashcardRepository
 {
-    public function create(string $question, string $answer, string $userId): Flashcard;
+    public function create(string $question, string $answer): Flashcard;
     
-    public function findById(int $id): Flashcard;
+    public function findById(int $id): ?Flashcard;
 } 
